1c27b3e5eb00f61d300b33ae2158ebdc2fb43400
Smug <zikadika2@gmail.com>
2024-09-16 20:53:03 -0400

    Added basic Spawn Function
    Added ant hill with basic spawn script,
    made prefab on queen/worker ant

---
+++ Assets/Scripts/AntHill.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntHill : MonoBehaviour
+{
+
+    public List<GameObject> antsSpawned = new List<GameObject>();
+    [SerializeField]
+    private GameObject ant, queen;
+    private float timer = 0f;
+    private int seconds = 0;
+
+    [SerializeField]
+    [Header("Number 1-10")]
+    private int spawnChance = 1;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject queenAnt = Instantiate(queen);
+        antsSpawned.Add(queenAnt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+        SpawnCheck();
+    }
+
+    private void Timer()
+    {
+        timer += Time.deltaTime;
+        seconds = (int)timer;
+        Debug.Log(seconds);
+    }
+    private void SpawnCheck()
+    {
+        if (seconds >= 30 )
+        {
+            timer = 0f;
+            int rng = Random.Range(1, 10);
+            if( spawnChance >= rng)
+            {
+                Spawn();
+            }
+        }
+    }
+    private void Spawn()
+    {
+        GameObject workerAnt = Instantiate(ant);
+        antsSpawned.Add(workerAnt);
+    }
+}
